35071d819d6ac70b47c633ea24dd0a95ab77f143
Xiaohan Liu <danae0730@gmail.com>
2022-01-15 18:59:36 -0800

    create a new table to handle user info

--- database/schema.sql
+++ database/schema.sql
@@ -1,19 +1,24 @@
-CREATE DATABASE mvp;
-CREATE TABLE info  (
-  id serial,
-  user_name text,
-  name text,
-  position text,
-  url text,
-  date DATE,
-  importance int,
-  status text,
-  comment text
-);
+-- CREATE DATABASE mvp;
+-- CREATE TABLE info  (
+--   id serial,
+--   user_name text,
+--   name text,
+--   position text,
+--   url text,
+--   date DATE,
+--   importance int,
+--   status text,
+--   comment text
+-- );
 
-CREATE INDEX user_index on info(user_name);
+-- CREATE TABLE user_info (
+--   uid text,
+--   name text
+-- );
+-- create index uid_index on user_info(uid);
+-- CREATE INDEX user_index on info(user_name);
+
 
 insert into info (user_name,name,position,url,date,importance,status)
-values ('user1', 'GOOGLE','software engineer','https://careers.google.com/jobs/results/126694548232381126/'
+values ('test', 'GOOGLE','software engineer','https://careers.google.com/jobs/results/126694548232381126/'
 ,'12/03/2021','5','Applied');
-
